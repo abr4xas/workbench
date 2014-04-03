eccaf8fde25d9ed2bcb64b1be0662b08ba87804b
abr4xas <me@abr4xas.org>
2014-04-03 14:13:33 -0430

    modificado

--- b2g.sh
+++ b2g.sh
@@ -11,8 +11,6 @@ git clone https://github.com/mozilla-b2g/B2G.git
 cd B2G
 ### Para actualizar el repo local al commit más nuevo.
 git pull
-### Sincronizamos
-./repo sync
 ### Para evitar un posible error que no pueda verificar la clave pública hacemos lo siguiente:
 ### Fuente: http://gnulinuxvagos.es/topic/2090-compilandoinstalando-la-última-versión-de-firefox-os-para-el-zte-open/
 curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ./repo
