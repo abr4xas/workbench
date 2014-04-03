974a5351d3df41dc07ed8f0c83b1815ee73029e5
abr4xas <me@abr4xas.org>
2014-04-03 13:46:04 -0430

    Archivo nuevo

---
+++ b2g.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Script para preparar el entorno para compilar una ROM para Firefox OS
+# Developed by abr4xas <me@abr4xas.org>
+
+echo "#################################"
+echo "#Obteniendo las fuentes de B2G ##"
+echo "#################################"
+
+### Clonamos el repo
+git clone https://github.com/mozilla-b2g/B2G.git
+cd B2G
+### Para actualizar el repo local al commit más nuevo.
+git pull
+### Sincronizamos
+./repo sync
+### Para evitar un posible error que no pueda verificar la clave pública hacemos lo siguiente:
+### Fuente: http://gnulinuxvagos.es/topic/2090-compilandoinstalando-la-última-versión-de-firefox-os-para-el-zte-open/
+curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ./repo
+chmod a+w repo
+echo "######################################################"
+echo "#                       Finalizado                  ##"
+echo "#                      Happy Hackin!                ##"
+echo "# BTW, no olvides comentar el repo de testing ;)    ##"	
+echo "######################################################"
