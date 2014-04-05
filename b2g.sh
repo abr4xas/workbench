600757eb650fc85c654b8c56dddfe62fe0179f61
abr4xas <me@abr4xas.org>
2014-04-05 08:09:37 -0430

    actualizados

--- b2g.sh
+++ b2g.sh
@@ -2,10 +2,9 @@
 # Script para preparar el entorno para compilar una ROM para Firefox OS
 # Developed by abr4xas <me@abr4xas.org>
 
-echo "#################################"
-echo "#Obteniendo las fuentes de B2G ##"
-echo "#################################"
-
+echo " "
+echo "         Obteniendo las fuentes de B2G           "
+echo " "
 ### Clonamos el repo
 git clone https://github.com/mozilla-b2g/B2G.git
 cd B2G
@@ -15,8 +14,8 @@ git pull
 ### Fuente: http://gnulinuxvagos.es/topic/2090-compilandoinstalando-la-última-versión-de-firefox-os-para-el-zte-open/
 curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ./repo
 chmod a+w repo
-echo "######################################################"
-echo "#                       Finalizado                  ##"
-echo "#                      Happy Hackin!                ##"
-echo "# BTW, no olvides comentar el repo de testing ;)    ##"	
-echo "######################################################"
+echo " "
+echo "              Finalizado                        "
+echo "              Happy Hackin!                     "
+echo "                    :)                          "
+echo " "
--- configure.sh
+++ configure.sh
@@ -17,7 +17,6 @@ dpkg --add-architecture i386
 apt-get update
 ### Solicitamos la instalación de los paquetes
 apt-get -y install git cmake autoconf2.13 bison bzip2 ccache curl flex gawk gcc g++ g++-multilib gcc-4.6 g++-4.6 g++-4.6-multilib git lib32ncurses5 lib32z1-dev libgl1-mesa-dev libx11-dev make zip libusb-1.0-0 libusb-1.0-0-dev zlib1g-dev:i386
-
 ### Usaremos GCC 4.6 ya que > a eso da problemas
 export CC=gcc-4.6
 export CXX=g++-4.6
@@ -30,30 +29,29 @@ echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", MODE="0666", GROUP="plugdev"' >>
 chmod a+r /etc/udev/rules.d/android.rules
 ### Reiniciamos el servivio UDEV
 service udev restart
-	echo "#################################################################"
-	echo "#Necesito las herramientas ADB (android debug bridge)          ##"
-	echo "#Por favor, agrega a tu sources.list el siguiente repo: 	     ##"
-	echo "#deb http://ftp.debian.org/debian testing main contrib non-free##"
-	echo "#Continuamos? Y/N  					     ##"	
-	echo "#################################################################"
+	echo "###################################################################"
+	echo "# Necesito las herramientas ADB (android debug bridge)           ##"
+	echo "# Por favor, agrega a tu sources.list el siguiente repo: 	       ##"
+	echo "# deb http://ftp.debian.org/debian testing main contrib non-free ##"
+	echo "# Continuamos? Y/N  					                           ##"	
+	echo "###################################################################"
 read continuar
 if [ $continuar = y ] ; then
-		echo "#########################################################"
-		echo "#Instalando las herramientas ADB (android debug bridge)##"
-		echo "#########################################################"
+		echo "###########################################################"
+		echo "# Instalando las herramientas ADB (android debug bridge) ##"
+		echo "###########################################################"
 ### Si agrego el repo de testing, actualizamos
 apt-get update
 ### E instalamos las herramientas ADB
 apt-get -y install android-tools-adb android-tools-fastboot
 
-		echo "#################################################################"
-		echo "#                       Finalizado                             ##"
-		echo "#                      Happy Hackin!                           ##"
-		echo "# BTW, no olvides comentar el repo de testing ;)               ##"	
-		echo "#################################################################"	
+		echo "#####################################################"
+		echo "#               Finalizado                         ##"
+		echo "#              Happy Hackin!                       ##"
+		echo "# BTW, no olvides comentar el repo de testing ;)   ##"	
+		echo "#####################################################"	
 	elif [ $continuar = n ] ; then
-		echo "#################################################################"
-		echo "#     Ha sido cancelado TODO el proceso por el usuario         ##"
-		echo "#                      :(                                      ##"
-		echo "#################################################################"		
+		echo "###########################################################"
+		echo "#  Ha sido cancelado TODO el proceso por el usuario :(   ##"
+		echo "###########################################################"		
 fi		
