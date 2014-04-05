119155c6816eb73ccb81f956523149215896545e
abr4xas <me@abr4xas.org>
2014-04-05 16:04:39 -0430

    typo

--- b2g.sh
+++ b2g.sh
@@ -8,14 +8,16 @@ echo " "
 ### Clonamos el repo
 git clone https://github.com/mozilla-b2g/B2G.git
 cd B2G
-### Para actualizar el repo local al commit más nuevo.
-git pull
+cp userconfig ~/B2G/.userconfig
 ### Para evitar un posible error que no pueda verificar la clave pública hacemos lo siguiente:
 ### Fuente: http://gnulinuxvagos.es/topic/2090-compilandoinstalando-la-última-versión-de-firefox-os-para-el-zte-open/
 curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ./repo
 chmod a+w repo
+### Para actualizar el repo local al commit más nuevo.
+git pull
 echo " "
 echo "              Finalizado                        "
-echo "              Happy Hackin!                     "
+echo "              Happy Hacking!                    "
 echo "                    :)                          "
 echo " "
+exit 0
--- configure.sh
+++ configure.sh
@@ -7,16 +7,18 @@ if [[ $USER != root ]]; then
 	echo "#################################################################"
 	echo "#         Error: Debe tener privilegios de ROOT                ##"
 	echo "#################################################################"
+	exit 1
 fi
 	echo "#################################################################"
 	echo "#              Agregando soporte Multiarch                     ##"
 	echo "#################################################################"
 ### Agregando soporte multiarch i386
 dpkg --add-architecture i386
+echo "deb http://ftp.debian.org/debian testing main contrib non-free" >> /etc/apt/sources.list
 ### Actualizamos
-apt-get update
+apt-get update -q 
 ### Solicitamos la instalación de los paquetes
-apt-get -y install git cmake autoconf2.13 bison bzip2 ccache curl flex gawk gcc g++ g++-multilib gcc-4.6 g++-4.6 g++-4.6-multilib git lib32ncurses5 lib32z1-dev libgl1-mesa-dev libx11-dev make zip libusb-1.0-0 libusb-1.0-0-dev zlib1g-dev:i386
+apt-get -y --force-yes install git cmake autoconf2.13 bison bzip2 ccache curl flex gawk gcc g++ g++-multilib gcc-4.6 g++-4.6 g++-4.6-multilib git lib32ncurses5 lib32z1-dev libgl1-mesa-dev libx11-dev make zip libusb-1.0-0 libusb-1.0-0-dev zlib1g-dev:i386 android-tools-adb android-tools-fastboot
 ### Usaremos GCC 4.6 ya que > a eso da problemas
 export CC=gcc-4.6
 export CXX=g++-4.6
@@ -29,29 +31,4 @@ echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", MODE="0666", GROUP="plugdev"' >>
 chmod a+r /etc/udev/rules.d/android.rules
 ### Reiniciamos el servivio UDEV
 service udev restart
-	echo "###################################################################"
-	echo "# Necesito las herramientas ADB (android debug bridge)           ##"
-	echo "# Por favor, agrega a tu sources.list el siguiente repo: 	       ##"
-	echo "# deb http://ftp.debian.org/debian testing main contrib non-free ##"
-	echo "# Continuamos? Y/N  					                           ##"	
-	echo "###################################################################"
-read continuar
-if [ $continuar = y ] ; then
-		echo "###########################################################"
-		echo "# Instalando las herramientas ADB (android debug bridge) ##"
-		echo "###########################################################"
-### Si agrego el repo de testing, actualizamos
-apt-get update
-### E instalamos las herramientas ADB
-apt-get -y install android-tools-adb android-tools-fastboot
-
-		echo "#####################################################"
-		echo "#               Finalizado                         ##"
-		echo "#              Happy Hackin!                       ##"
-		echo "# BTW, no olvides comentar el repo de testing ;)   ##"	
-		echo "#####################################################"	
-	elif [ $continuar = n ] ; then
-		echo "###########################################################"
-		echo "#  Ha sido cancelado TODO el proceso por el usuario :(   ##"
-		echo "###########################################################"		
-fi		
+exit 0
