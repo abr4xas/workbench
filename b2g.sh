d82910195fc300c2d032810295e923bfe4b18655
abr4xas <me@abr4xas.org>
2014-04-08 12:49:33 -0430

    Fix git buffer is too low

--- b2g.sh
+++ b2g.sh
@@ -6,11 +6,11 @@
 echo " "
 echo "         Obteniendo las fuentes de B2G           "
 echo " "
-### You will need to increase Git’s HTTP buffer by setting the git config var
-git config --global http.postBuffer 5242880000
 ### Clonamos el repo
 git clone https://github.com/mozilla-b2g/B2G.git
 cd B2G
+### You will need to increase Git’s HTTP buffer by setting the git config var
+git config http.postBuffer 5242880000
 cp userconfig ~/B2G/.userconfig
 ### Para evitar un posible error que no pueda verificar la clave pública hacemos lo siguiente:
 ### Fuente: http://gnulinuxvagos.es/topic/2090-compilandoinstalando-la-última-versión-de-firefox-os-para-el-zte-open/
