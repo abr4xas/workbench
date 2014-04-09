0e8f48b02f6b3e324c22e256186de658e30bad69
ángel <bullgram@gmail.com>
2014-04-09 10:39:43 -0430

    Update b2g.sh

--- b2g.sh
+++ b2g.sh
@@ -11,7 +11,7 @@ git clone https://github.com/mozilla-b2g/B2G.git
 cd B2G
 ### You will need to increase Git’s HTTP buffer by setting the git config var
 git config http.postBuffer 5242880000
-cp userconfig ~/B2G/.userconfig
+cp ~/userconfig ~/B2G/.userconfig
 ### Para evitar un posible error que no pueda verificar la clave pública hacemos lo siguiente:
 ### Fuente: http://gnulinuxvagos.es/topic/2090-compilandoinstalando-la-última-versión-de-firefox-os-para-el-zte-open/
 curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ./repo
